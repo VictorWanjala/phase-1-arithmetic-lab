94548454a4481be253154b800e0ee763f8733834
Victor <wanjala94vic@gmail.com>
2023-07-11 00:02:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 100);
+
+let num3 = 25;
+let num4 = 7;
+
+let mod = num3 % num4;
+let numberSet = [3,5,20,7,8];
+
+let max = Math.max(...numberSet);
